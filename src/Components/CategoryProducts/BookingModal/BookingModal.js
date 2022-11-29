2b9ad7a12eb3b167677de7d423717c3c707d657e
AsaduzzamanAshik1610 <mdashik1610@gmail.com>
2022-11-29 18:35:49 +0600

    updated the footer

--- src/Components/CategoryProducts/BookingModal/BookingModal.js
+++ src/Components/CategoryProducts/BookingModal/BookingModal.js
@@ -10,12 +10,12 @@ const BookingModal = ({product}) => {
                     <label htmlFor="Booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                     <h3 className="text-lg font-bold">{Brand}</h3>
                     <form className='grid grid-cols-1 gap-3 mt-6'>
-                    <input type="text" placeholder="Type here" className="input w-full" />
-                    <input type="text" placeholder="Type here" className="input w-full" />
-                    <input type="text" placeholder="Type here" className="input w-full" />
-                    <input type="text" placeholder="Type here" className="input w-full" />
+                    <input type="text" placeholder="Type here" className="input w-full input-bordered" />
+                    <input type="text" placeholder="Type here" className="input w-full input-bordered" />
+                    <input type="text" placeholder="Type here" className="input w-full input-bordered" />
+                    <input type="text" placeholder="Type here" className="input w-full input-bordered" />
                     <br />
-                    <input className='btn btn-accent w-full max-w-xs' type="submit" value="submit" />
+                    <input className='btn btn-accent w-full input-bordered max-w-xs' type="submit" value="submit" />
                     </form>
                 </div>
             </div>
