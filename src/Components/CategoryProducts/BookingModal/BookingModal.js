1fd82fc4f4f149faf446d435ea252190b2846594
AsaduzzamanAshik1610 <mdashik1610@gmail.com>
2022-11-29 19:47:41 +0600

    added the product submmit

--- src/Components/CategoryProducts/BookingModal/BookingModal.js
+++ src/Components/CategoryProducts/BookingModal/BookingModal.js
@@ -2,6 +2,20 @@ import React from 'react';
 
 const BookingModal = ({product}) => {
     const {Brand} = product;
+    const handleBooking = event =>{
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const phone = form.phone.value;
+        const booking ={
+            name,
+            email,
+            phone,
+        }
+        console.log(booking);
+        
+    }
     return (
         <>
             <input type="checkbox" id="Booking-modal" className="modal-toggle" />
@@ -9,11 +23,10 @@ const BookingModal = ({product}) => {
                 <div className="modal-box relative">
                     <label htmlFor="Booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                     <h3 className="text-lg font-bold">{Brand}</h3>
-                    <form className='grid grid-cols-1 gap-3 mt-6'>
-                    <input type="text" placeholder="Type here" className="input w-full input-bordered" />
-                    <input type="text" placeholder="Type here" className="input w-full input-bordered" />
-                    <input type="text" placeholder="Type here" className="input w-full input-bordered" />
-                    <input type="text" placeholder="Type here" className="input w-full input-bordered" />
+                    <form onSubmit={handleBooking} className='grid grid-cols-1 gap-3 mt-6'>
+                    <input name='name' type="text" placeholder="Your Name" className="input w-full input-bordered" />
+                    <input name='email' type="email" placeholder="Email Address" className="input w-full input-bordered" />
+                    <input name='phone' type="text" placeholder="Phone Number" className="input w-full input-bordered" />
                     <br />
                     <input className='btn btn-accent w-full input-bordered max-w-xs' type="submit" value="submit" />
                     </form>
